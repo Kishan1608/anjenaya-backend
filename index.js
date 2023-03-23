071d0853b8dda3165e13c5a3e6517e3a68cd57be
chauh <chauhankishan1608@gmail.com>
2023-03-23 13:10:26 +0530

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://admin-kishan:admin-kishan@cluster0.kpnxf.mongodb.net/AnjenayaInvestments?retryWrites=true&w=majority", {
+mongoose.connect(process.env.uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB')).catch(err=>console.log(err));
